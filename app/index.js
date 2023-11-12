3e656d9c64dd6a2a5ae5a40791e5a9fe9d9b42d8
Joyal A Johney <joyalajohny@gmail.com>
2023-11-12 18:22:38 +0530

    implement layer 7 loadbalancing

--- app/index.js
+++ app/index.js
@@ -1,9 +1,16 @@
-const express = require("express");
-const app = express();
-const os = require("os")
-const hostname = os.hostname();
+const app = require("express")();
+const appid = process.env.APPID;
 
+app.get("/", (req,res) => 
+res.send(`appid: ${appid} home page: says hello!`))
 
-app.get("/", (req, res) => res.send("Hello from a lightweight container") ) ;
-app.listen(9999, () => console.log("listening on port 9999 on " + hostname));
-
+app.get("/app1", (req,res) => 
+res.send(`appid: ${appid} app1 page: says hello!`))
+ 
+app.get("/app2", (req,res) => 
+res.send(`appid: ${appid} app2 page: says hello!`))
+ 
+app.get("/admin", (req,res) => 
+res.send(`appid: ${appid} ADMIN page: very few people should see this`))
+ 
+app.listen(9999, ()=>console.log(`${appid} is listening on 9999`))
